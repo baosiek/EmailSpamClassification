7494dc26d574b234fe1ab9e23f66b6dd9af0438e
Bruno Adam Osiek <baosiek@gmail.com>
2020-11-04 11:17:54 -0800

    Curating dataset to spam classification

--- test_email_spam.py
+++ test_email_spam.py
@@ -1,16 +1,12 @@
 import unittest
 
 
-def porra(self, caralho):
-    print(caralho)
-    
-    
 def _print_success_message():
     print("Test passed!")
     
-class Tests(unittest.TestCase):
+class TestEmailSpams(unittest.TestCase):
 
-    def test_sizess(self):    
+    def test_sizes(self):    
         with open("./data/contents.txt", "rb") as fp:   # Deserializing contents
             contents = pickle.load(fp)
             self.assertEqual(len(contents), 517401)
